4387f186736a3b5cdafa3aaaf92993ef8cf3b8bc
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-08-11 12:11:33 +0530

    Add dat.gui and modify the plane geometry properties by dat.gui

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10
+  }
+};
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.
+      PlaneGeometry(
+        world.plane.width, 
+        world.plane.height, 
+        world.plane.widthSegments,
+        world.plane.heightSegments
+      );
+    const { array } = planeMesh.geometry.attributes.position;
+
+    for (let i = 0; i < array.length; i += 3) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
+
+      array[i + 2] = z + Math.random();
+    };
+}
+
+gui.add(world.plane, 'width', 1, 20)
+  .onChange(generatePlane);
+
+
+
+gui.add(world.plane, 'height', 1, 20)
+  .onChange(generatePlane);
+
+gui.add(world.plane, 'widthSegments', 1, 50)
+  .onChange(generatePlane);
+
+
+gui.add(world.plane, 'heightSegments', 1, 50)
+  .onChange(generatePlane);
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 
