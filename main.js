acedec1eb209ce63e061eaa5e4ecbafdd90f1037
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-08-11 11:28:42 +0530

    Add directional light and Phong Material to plane mesh

--- main.js
+++ main.js
@@ -17,11 +17,17 @@ camera.position.z = 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xFF0000, side: THREE.DoubleSide});
+const planeMaterial = new THREE.MeshPhongMaterial({color: 0xFF0000, side: THREE.DoubleSide});
 
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(planeMesh);
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+
+light.position.set(0, 0, 1);
+
+scene.add(light);
 
 function animate() {
   requestAnimationFrame(animate);
