05c2742c11ae4bccf4c202f150a443968fe8692d
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-08-14 16:58:18 +0530

    Scene Responsiveness and Browser Resizing

--- main.js
+++ main.js
@@ -266,3 +266,9 @@ document.querySelector('#myWorkBtn')
       delay: 2
     });
   });
+
+  addEventListener('resize', () => {
+    camera.aspect = innerWidth/innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(innerWidth, innerHeight);    
+  });
